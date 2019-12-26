ad463fd4e1d88435ad1a5728eef90ffb362520fb
monotone-RK <kobaya4121@gmail.com>
2019-12-26 19:21:12 +0900

    version change (18 -> 19)

--- interfpga_comm/main.cc
+++ interfpga_comm/main.cc
@@ -1,21 +1,31 @@
-#include <stdint.h>
 #include <random>
-#include <time.h>
-#include <stdlib.h>
 #include <mpi.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/mman.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#define __CL_ENABLE_EXCEPTIONS
-#include <CL/cl.hpp>
-#include <iostream>
-#include <fstream>
-#include <stdint.h>
-#include <math.h>
 
-#define CALL(cq, k, ...) k.bind(cq, cl::NDRange(1), cl::NDRange(1))(__VA_ARGS__)
+#ifndef CL_HPP_ENABLE_EXCEPTIONS
+#  define CL_HPP_ENABLE_EXCEPTIONS
+#endif
+#ifndef CL_TARGET_OPENCL_VERSION
+#  define CL_TARGET_OPENCL_VERSION 200
+#endif
+#ifndef CL_HPP_TARGET_OPENCL_VERSION
+#  define CL_HPP_TARGET_OPENCL_VERSION 200
+#endif
+#ifndef CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#  define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#endif
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wignored-qualifiers"
+#pragma GCC diagnostic ignored "-Wunused-function"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#include <CL/cl.h>
+#include <CL/cl2.hpp>
+#include <CL/cl_ext_intelfpga.h>
+#pragma GCC diagnostic pop
 
 int main(int argc, char** argv) {
 
@@ -29,76 +39,122 @@ int main(int argc, char** argv) {
   
   fprintf(stderr, "%d/%d\n", rank, size);
 
-  ///// Create platform //////
-  std::vector<cl::Platform> ps;
-  cl::Platform::get(&ps);
-  cl::Platform platform = ps.at(0);
+  MPI_Status status;
 
-  ///// Create context //////
-  std::vector<cl::Device> dev;
-  platform.getDevices(CL_DEVICE_TYPE_ALL, &dev);
-  cl::Context ctx = cl::Context(dev);
-  
+  ///// Create platform //////
+  cl::Platform platform;
+  std::vector<cl::Platform> platforms;
+  cl::Platform::get(&platforms);
+
+  for (auto &p : platforms) {
+    auto const name = p.getInfo<CL_PLATFORM_NAME>();
+    if (name.find("Intel(R) FPGA SDK for OpenCL(TM)") != std::string::npos) {
+      platform = p;
+      break;
+    }
+  }
+
+  if (!platform()) {
+    throw cl::Error(CL_DEVICE_NOT_FOUND, "Platform not found");
+  }
+  if (cl::Platform::setDefault(platform) != platform) {
+    throw cl::Error(CL_DEVICE_NOT_FOUND, "Platform not found");
+  }
+
   ///// Check aocx //////
-  int const fd = open(argv[1], O_RDONLY);
-  if (fd < 0) {
+  auto const fd = open(argv[1], O_RDONLY);
+  if (fd == -1) {
     perror("open");
-    exit(1);
+    throw cl::Error(CL_INVALID_PROGRAM_EXECUTABLE, "open(2)");
   }
 
   struct stat st;
+
   if (fstat(fd, &st)) {
-    perror("stat");
-    exit(1);
+    throw cl::Error(CL_INVALID_PROGRAM_EXECUTABLE, "fstat(2)");
   }
 
-  void* aocx = calloc(1, st.st_size);
-  ssize_t nr = read(fd, aocx, st.st_size);
-
-  if (nr < 0 || nr != st.st_size) {
-    perror("read");
-    exit(1);
+  auto data = new char[st.st_size];
+  if (read(fd, data, st.st_size) != st.st_size) {
+    throw cl::Error(CL_INVALID_PROGRAM_EXECUTABLE, "read(2)");
   }
-  
+
+  ///// Create context //////
+  int dev_idx = 0;
+  char const* str;
+  str = getenv("OMPI_COMM_WORLD_LOCAL_RANK");
+  if (str) {
+    dev_idx = atoi(str);
+  }
+
+  std::vector<cl::Device> devs;
+  platform.getDevices(CL_DEVICE_TYPE_ALL, &devs);
+  auto const& dev = devs.at(dev_idx);
+  auto ctx = cl::Context{dev};
+
+  cl::Context::setDefault(ctx);
+  cl::Device::setDefault(dev);
+
+  fprintf(stderr, "**** platform = %s, dev_idx = %d ****\n", platform.getInfo<CL_PLATFORM_NAME>().c_str(), dev_idx);
+
   ///// Create program /////
-  cl::Program::Binaries bins{{ aocx, st.st_size }};
-  cl::Program prog(ctx, dev, bins);
-  
+  auto dev_cl = dev();
+  auto len = static_cast<size_t>(st.st_size);
+  auto image = (const unsigned char *)data;
+  cl_int error;
+
+  auto prg = clCreateProgramWithBinary(ctx(), 1, &dev_cl, &len, &image, nullptr, &error);
+  cl::detail::errHandler(error, "clCreateProgramWithBinary");
+
   ///// Create command queue /////
-  cl::CommandQueue cq0(ctx, dev.at(0));
-  cl::CommandQueue cq1(ctx, dev.at(0));
+  cl::CommandQueue cq0(ctx, dev);
+  cl::CommandQueue cq1(ctx, dev);
     
   ///// Create kernel /////
-  cl::Kernel send(prog, "send");
-  cl::Kernel recv(prog, "recv");
-    
-  cl_float8 *h_send;
-  cl_float8 *h_recv;
-  size_t const numdata = 1;
-  size_t const BUF_SIZE = sizeof(cl_float8) * numdata;
+  cl::Kernel k_send(cl::Program(prg, true), "send");
+  cl::Kernel k_recv(cl::Program(prg, true), "recv");
 
+  ///// Create kernel functor /////
+  cl::KernelFunctor<cl::Buffer, cl_int, cl_int> f_send(k_send);
+  cl::KernelFunctor<cl::Buffer, cl_int, cl_int> f_recv(k_recv);
+    
+  ///// Create buffer (for host and device) /////
+  size_t const numdata = 1;
+  size_t const BUF_SIZE = sizeof(cl_float16) * numdata;
+
+  // host
+  cl_float16 *h_send;
+  cl_float16 *h_recv;
   posix_memalign((void**)&h_send, 64, BUF_SIZE);
   posix_memalign((void**)&h_recv, 64, BUF_SIZE);
-
-  ///// Create buffer /////
-  cl::Buffer d_send(ctx, CL_MEM_READ_WRITE, BUF_SIZE);
-  cl::Buffer d_recv(ctx, CL_MEM_READ_WRITE, BUF_SIZE);
-  
   memset(h_send, 0x0, BUF_SIZE);
   memset(h_recv, 0x0, BUF_SIZE);
 
+  // device
+  cl::Buffer d_send(ctx, CL_MEM_READ_WRITE, BUF_SIZE);
+  cl::Buffer d_recv(ctx, CL_MEM_READ_WRITE, BUF_SIZE);
+
+  ///// Set init data /////
   std::mt19937 g(12345);
   std::uniform_real_distribution<float> d;
 
   for (int i = 0; i < (int)numdata; i++) {
-    h_send[i].s[0] = d(g);
-    h_send[i].s[1] = d(g);
-    h_send[i].s[2] = d(g);
-    h_send[i].s[3] = d(g);
-    h_send[i].s[4] = d(g);
-    h_send[i].s[5] = d(g);
-    h_send[i].s[6] = d(g);
-    h_send[i].s[7] = d(g);
+    h_send[i].s[ 0] = d(g);
+    h_send[i].s[ 1] = d(g);
+    h_send[i].s[ 2] = d(g);
+    h_send[i].s[ 3] = d(g);
+    h_send[i].s[ 4] = d(g);
+    h_send[i].s[ 5] = d(g);
+    h_send[i].s[ 6] = d(g);
+    h_send[i].s[ 7] = d(g);
+    h_send[i].s[ 8] = d(g);
+    h_send[i].s[ 9] = d(g);
+    h_send[i].s[10] = d(g);
+    h_send[i].s[11] = d(g);
+    h_send[i].s[12] = d(g);
+    h_send[i].s[13] = d(g);
+    h_send[i].s[14] = d(g);
+    h_send[i].s[15] = 0;  // must be zero
   }
   
   switch (rank) {
@@ -114,20 +170,63 @@ int main(int argc, char** argv) {
 
   MPI_Barrier(MPI_COMM_WORLD);
 
-  // /////////////////////////////////////////////////////////////////////
+  ///// Run inter-FPGA comm /////
+  /////////////////////////////////////////////////////////////////////
   fprintf(stderr, "OpenCL kernel activated (from %d)\n", rank);
   switch (rank) {
     case 0:
-      CALL(cq0, send, d_send, (int)numdata, rank);
+      fprintf(stderr, "h_send: %f at rank 0 (before)\n", h_send[0].s[0]);
+      f_send(cl::EnqueueArgs(cq0, cl::NDRange(1), cl::NDRange(1)), d_send, (int)numdata, rank);
       cq0.finish();
       break;
     case 1:
-      CALL(cq1, recv, d_recv, (int)numdata, rank);
+      fprintf(stderr, "h_recv: %f at rank 1 (before)\n", h_recv[0].s[0]);
+      f_recv(cl::EnqueueArgs(cq1, cl::NDRange(1), cl::NDRange(1)), d_recv, (int)numdata, rank);
       cq1.finish();
       break;
   }
   fprintf(stderr, "OpenCL kernel terminated (from %d)\n", rank);
-  // /////////////////////////////////////////////////////////////////////  
+  /////////////////////////////////////////////////////////////////////
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  MPI_Datatype float16;
+  MPI_Type_contiguous(16, MPI_FLOAT, &float16);
+  MPI_Type_commit(&float16);
+
+  switch (rank) {
+    case 0:
+      MPI_Recv(h_recv, numdata, float16, 1, 0, MPI_COMM_WORLD, &status);
+      fprintf(stderr, "h_recv: %f at rank 0 (after)\n", h_recv[0].s[0]);
+      break;
+    case 1:
+      cq1.enqueueReadBuffer(d_recv, CL_TRUE, 0, BUF_SIZE, h_recv);
+      cq1.finish();
+      fprintf(stderr, "h_recv: %f at rank 1 (after)\n", h_recv[0].s[0]);
+      MPI_Send(h_recv, numdata, float16, 0, 0, MPI_COMM_WORLD);
+      break;
+  }
+
+  if (rank == 0) {
+    for (int i = 0; i < (int)numdata; i++) {
+      if (h_send[i].s[ 0] != h_recv[i].s[ 0]) fprintf(stderr, "ERROR at 0\n");
+      if (h_send[i].s[ 1] != h_recv[i].s[ 1]) fprintf(stderr, "ERROR at 1\n");
+      if (h_send[i].s[ 2] != h_recv[i].s[ 2]) fprintf(stderr, "ERROR at 2\n");
+      if (h_send[i].s[ 3] != h_recv[i].s[ 3]) fprintf(stderr, "ERROR at 3\n");
+      if (h_send[i].s[ 4] != h_recv[i].s[ 4]) fprintf(stderr, "ERROR at 4\n");
+      if (h_send[i].s[ 5] != h_recv[i].s[ 5]) fprintf(stderr, "ERROR at 5\n");
+      if (h_send[i].s[ 6] != h_recv[i].s[ 6]) fprintf(stderr, "ERROR at 6\n");
+      if (h_send[i].s[ 7] != h_recv[i].s[ 7]) fprintf(stderr, "ERROR at 7\n");
+      if (h_send[i].s[ 8] != h_recv[i].s[ 8]) fprintf(stderr, "ERROR at 8\n");
+      if (h_send[i].s[ 9] != h_recv[i].s[ 9]) fprintf(stderr, "ERROR at 9\n");
+      if (h_send[i].s[10] != h_recv[i].s[10]) fprintf(stderr, "ERROR at 10\n");
+      if (h_send[i].s[11] != h_recv[i].s[11]) fprintf(stderr, "ERROR at 11\n");
+      if (h_send[i].s[12] != h_recv[i].s[12]) fprintf(stderr, "ERROR at 12\n");
+      if (h_send[i].s[13] != h_recv[i].s[13]) fprintf(stderr, "ERROR at 13\n");
+      if (h_send[i].s[14] != h_recv[i].s[14]) fprintf(stderr, "ERROR at 14\n");
+      if (h_send[i].s[15] != h_recv[i].s[15]) fprintf(stderr, "ERROR at 15\n");
+    }
+  }
 
   MPI_Barrier(MPI_COMM_WORLD);
 
--- interfpga_comm/test.cl
+++ interfpga_comm/test.cl
@@ -1,20 +1,20 @@
 #pragma OPENCL EXTENSION cl_intel_channels : enable
 
-/* channel float8 simple_ot __attribute__((depth(0), io("kernel_output_ch0"))); */
-/* channel float8 simple_in __attribute__((depth(0), io("kernel_input_ch1"))); */
+channel float16 simple_ot __attribute__((depth(0), io("tx0")));
+channel float16 simple_in __attribute__((depth(0), io("rx1")));
 
-__kernel void send(__global float8* restrict data, int n, int rank) {
+__kernel void send(__global float16* restrict data, int n, int rank) {
   for (int i = 0; i < n; i++) {
-    float8 v = data[i];
-    /* write_channel_intel(simple_ot, v); */
-    printf("At send (rank: %d)\n", rank);
+    float16 v = data[i];
+    write_channel_intel(simple_ot, v);
+    /* printf("At send (rank: %d)\n", rank); */
   }
 }
 
-__kernel void recv(__global float8* restrict data, int n, int rank) {
+__kernel void recv(__global float16* restrict data, int n, int rank) {
   for (int i = 0; i < n; i++) {
-    /* float8 v = read_channel_intel(simple_in); */
-    /* data[i] = v; */
-    printf("At recv (rank: %d)\n", rank);    
+    float16 v = read_channel_intel(simple_in);
+    data[i] = v;
+    /* printf("At recv (rank: %d)\n", rank);     */
   }
 }
